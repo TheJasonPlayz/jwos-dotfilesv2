90170bbdee5ddd1ed6a2ea6c7197471b53d1e59a
TheJasonPlayz <thejasonplayz@gmail.com>
2022-12-05 21:00:47 -0700

    automatic script update

--- stow/home/.emacs.d/init.el
+++ stow/home/.emacs.d/init.el
@@ -161,7 +161,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(csv-mode which-key use-package rainbow-delimiters org-auto-tangle magit lua-mode helpful haskell-mode exwm counsel)))
+   '(vterm csv-mode which-key use-package rainbow-delimiters org-auto-tangle magit lua-mode helpful haskell-mode exwm counsel)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
